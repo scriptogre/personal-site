08c2255303694b3b95e5b4ae741f14573e5393a8
eliasrestea <eliasrestea@gmail.com>
2023-11-15 18:24:26 +0200

    Feat: added vercel support and fixed multiple bugs
    -javascript code fixed to work with view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  inlineStylesheets: 'never',
   site: 'https://eliasrestea.github.io/',
   base: '/',
+  output: "server",
+  adapter: vercel()
 });
