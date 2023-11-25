7abe0e389bd5c0082f7cfb3759ae8fffeee9f6e7
Chris <106525874+scriptogre@users.noreply.github.com>
2023-11-25 15:24:45 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://eliasrestea.github.io/',
+  site: 'https://scriptogre.github.io/',
   base: '/',
   output: "server",
   adapter: vercel()
